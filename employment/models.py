61d6461491a0abff495a36654bf0092e1c69fec5
nkurudavid <nkurudavid44@gmail.com>
2023-04-12 20:34:07 +0200

    created  models

--- employment/models.py
+++ employment/models.py
@@ -1,4 +1,43 @@
 from django.db import models
+from django.urls import reverse
 from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Department(models.Model):
+    department = models.CharField(max_length=100, blank=False, unique=True)
+    class Meta:
+        verbose_name = "Department"
+        verbose_name_plural = "Departments"
+        ordering = ['department']
+
+    def __str__(self):
+        return self.department
+
+class Employee(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, blank=False, unique=True)
+    last_name = models.CharField(max_length=100, blank=False, unique=True)
+    email = models.EmailField(max_length=100, blank=False, unique=True)
+    phone_number = PhoneNumberField(blank=False, unique=True)
+    dateJoined = models.DateTimeField(auto_now_add=True, blank=False)
+    profile_picture = models.ImageField(upload_to="profile_picture")
+    class Meta:
+        verbose_name = "Employee"
+        verbose_name_plural = "Employees"
+        ordering = ['first_name']
+    def __str__(self):
+        return "{} {}".format(self.first_name,self.last_name)
+
+    def get_absolute_url(self):
+        return reverse('employee-detail', kwargs={'pk': self.pk})
+    def get_full_name(self):
+        return "{} {}".format(self.first_name,self.last_name)
+    def get_short_name(self):
+        return "{} {}".format(self.first_name,self.last_name)
+    def get_department(self):
+        return self.department.department
+    def get_phone_number(self):
+        return self.phone_number
+    def get_email(self):
+        return self.email
+
