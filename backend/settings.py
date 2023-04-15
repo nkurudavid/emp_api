b32dcd5a6f8b7de89bcdc6cce2335b79534b1679
nkurudavid <nkurudavid44@gmail.com>
2023-04-15 04:19:08 +0200

    Extracted secrets from the settings file

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 import environ
 env = environ.Env(
     # set casting, default value
@@ -19,6 +22,7 @@ env = environ.Env(
 )
 # reading .env file
 environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
